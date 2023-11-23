2d64c727595c0470457f9645caadfff85752d0a9
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2023-11-23 10:32:28 +0300

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
 </script>
 
 <template>
   <div class="container">
-    <Greet />
+    <select id="select">
+      <option hidden>Select item</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+    </select>
+
+    <input id="input" type="text" >
+    
+    <button id="button">Save</button>
   </div>
 </template>
 
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
-</style>
