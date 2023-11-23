341c4cc3277f3edd1bc3af6db1d0722c58c8d102
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2023-11-23 16:00:23 +0300

    edit app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+<script>
+import { invoke } from '@tauri-apps/api/tauri'
+
+export default {
+  data() {
+    return {
+      selected: [],
+    }
+  },
+  methods: {
+    async save() {
+      await invoke('save');
+    }
+  },
+  mounted() {
+    this.save();
+  }
+}
+
 </script>
 
 <template>
   <div class="container">
-    <select id="select">
+    <select id="select" :on-change="selected">
       <option hidden>Select item</option>
       <option value="1">One</option>
       <option value="2">Two</option>
     </select>
 
-    <input id="input" type="text" placeholder="Select item" >
-    
-    <button id="button">Save</button>
+    <input id="input" type="text" :value="selected" placeholder="Select item">
+
+    <button id="button" @click="save">Save</button>
   </div>
 </template>
 
