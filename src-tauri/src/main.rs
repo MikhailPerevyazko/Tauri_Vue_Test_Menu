ac10fab281068fd46cc63535d677076bd02abeaa
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2023-11-24 15:52:46 +0300

    add serde/create option

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,27 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-
-#[tauri::command]
-fn save() {
-    println!("Hello");
-}
+use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![save])
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Debug)]
+
+struct ItemsInput {
+    x: String,
+    y: String,
 }
--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,28 @@
-<script>
-import { invoke } from '@tauri-apps/api/tauri'
+<script setup>
+const props = defineProps({
+  selected: {default: () => (0) }
+})
 
-export default {
-  data() {
-    return {
-      selected: [],
-    }
-  },
-  methods: {
-    async save() {
-      await invoke('save');
-    }
-  },
-  mounted() {
-    this.save();
-  }
+const options = [
+  { name: 'select text', id: 0 },
+  { name: 'book1', id: 1 },
+  { name: 'book2', id: 2 },
+  { name: 'book3', id: 3 }
+];
+const emit = defineEmits(['update:selected'])
+
+function inputSignal(e) {
+  emit('update:selected',e.target.value)
 }
-
 </script>
 
 <template>
   <div class="container">
-    <select id="select" :on-change="selected">
-      <option hidden>Select item</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
+    <select id="select" @change="inputSignal" :value="selected">
+        <option v-for = "option in options" :value="option.id">{{ option.name }}</option>
     </select>
-
-    <input id="input" type="text" :value="selected" placeholder="Select item">
-
-    <button id="button" @click="save">Save</button>
+    <input id="input" type="text" :value="selected" placeholder="Select item"/>
+    <button id="button">Save</button>
   </div>
 </template>
 
